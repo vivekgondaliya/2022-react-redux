b2d72470487d7352d0adacb18acbd4f8c6a461ad
Vivek Gondalia <vivekgondaliya@gmail.com>
2022-08-30 12:06:12 +0530

    Redux : useSelector() to use state

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,5 +1,9 @@
+import {useSelector} from "react-redux";
+
 
 const ProductListing = () => {
+    const products = useSelector((state) => state);
+    console.log(products);
     return (
         <h1>Product Listing</h1>
     )
